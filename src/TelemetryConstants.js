8084a6825e7afea14769e3a5e0f32d4d73104bdc
pranjal bhadu <pranjalbhadu171@gmail.com>
2022-07-20 11:57:03 +0530

    added

--- src/TelemetryConstants.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TelemetryConstants = void 0;
-class TelemetryConstants {
-}
-exports.TelemetryConstants = TelemetryConstants;
-TelemetryConstants.ServiceName = "Telemetry.Instrumentation";
-TelemetryConstants.ServiceVersion = "0.1.0";
-//# sourceMappingURL=TelemetryConstants.js.map
